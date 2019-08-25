608effaf3ee33ac4344f3c4f744f4699038e1768
IPAndreev <aip1990andreev@yandex.ru>
2019-08-25 22:51:16 +0300

    Change in task 16

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -7,19 +7,6 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-//            File file = new File("aplana.txt");
-//            if (!file.exists())
-//                file.createNewFile();
-//            FileWriter writer = new FileWriter(file);
-//
-//            Scanner scanner = new Scanner(System.in);
-//            System.out.println("Введите текст для записи в файл: ");
-//            String str = scanner.nextLine();
-//
-//            writer.write(str);
-//            writer.flush();
-//            writer.close();
-
             BufferedReader br = new BufferedReader(new FileReader("aplana.txt"));
 
             String  textOutput;
